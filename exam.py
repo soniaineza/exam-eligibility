c496f60d058593fb88eb1a4022917033d60f1776
soniaineza <ntwariprince013@gmail.com>
2024-11-14 18:47:49 +0200

    eligibility

---
+++ exam.py
@@ -0,0 +1,5 @@
+marks = float(input("Enter your average marks: "))
+if marks >= 40:
+    print("You are eligible to take the exam!")
+else:
+    print("Sorry, you are not eligible to take the exam.")
